55618f317fb52229d2889b248d6421d7fd5c2a58
Hannes Eilers <mail@hanneseilers.de>
2018-06-05 01:02:23 +0200

    Added algorithm to automatically load modules from path.

--- NAO Communicator Server/src/commands/Command.py
+++ NAO Communicator Server/src/commands/Command.py
@@ -1,6 +1,8 @@
 from commands.usrcommands import * 			# @UnusedWildImport
 from thread import start_new_thread
 import logging
+import os
+import inspect
 import commands.usrcommands
 
 class NAOCommand(object):
@@ -19,41 +21,35 @@ class NAOCommand(object):
 		"""
 		Adds commands to a list (NAOCommand.lst) and returns that list
 		"""
-		logging.info( "Importing commands %s", str(commands.usrcommands.__all__) )
-		NAOCommand.lst.append( cmdSetSystemVolume.cmdSetSystemVolume() )
-		NAOCommand.lst.append( cmdSetPlayerVolume.cmdSetPlayerVolume() )
-		NAOCommand.lst.append( cmdSetSpeechVolume.cmdSetSpeechVolume() )
-		NAOCommand.lst.append( cmdSetSpeechLanguage.cmdSetSpeechLanguage() )
-		NAOCommand.lst.append( cmdSetSpeechVoice.cmdSetSpeechVoice() )
 		
-		NAOCommand.lst.append( cmdSetJointStiffness.cmdSetJointStiffness() )
-		NAOCommand.lst.append( cmdMemoryEventRaise.cmdMemoryEventRaise() )
-		NAOCommand.lst.append( cmdMemoryEventAdd.cmdMemoryEventAdd() )
-		NAOCommand.lst.append( cmdMemoryEventRemove.cmdMemoryEventRemove() )
-		NAOCommand.lst.append( cmdPlayProgram.cmdPlayProgram() )
-		NAOCommand.lst.append( cmdStopProgram.cmdStopProgram() )
-		NAOCommand.lst.append( cmdGetProgramStatus.cmdGetProgramStatus() )
+		# load modules dynamicaly
+		dirs = ["commands/usrcommands"]				# list of dirs to search for modules
+		for d in dirs:
+			logging.info( "searching for modules in %s", d )
 		
-		NAOCommand.lst.append( cmdOpenHand.cmdOpenHand() )
-		NAOCommand.lst.append( cmdSetLifeState.cmdSetLifeState() )
-		NAOCommand.lst.append( cmdSetNaoName.cmdSetNaoName() )			
-		
-		NAOCommand.lst.append( cmdSay.cmdSay() )
-		NAOCommand.lst.append( cmdStandUp.cmdStandUp() )
-		NAOCommand.lst.append( cmdSitDown.cmdSitDown() )
-		NAOCommand.lst.append( cmdVelocityWalk.cmdVelocityWalk() )
-		
-		NAOCommand.lst.append( ledAngry.ledAngry() )
-		NAOCommand.lst.append( ledBlink.ledBlink() )
-		NAOCommand.lst.append( ledCautious.ledCautious() )
-		NAOCommand.lst.append( ledCircleEyes.ledCircleEyes() )
-		NAOCommand.lst.append( ledDisco.ledDisco() )
-		NAOCommand.lst.append( ledFlash.ledFlash() )
-		NAOCommand.lst.append( ledHappy.ledHappy() )
-		NAOCommand.lst.append( ledLaugh.ledLaugh() )
-		NAOCommand.lst.append( ledMischievious.ledMischievious() )
-		NAOCommand.lst.append( ledThinking.ledThinking() )
-		NAOCommand.lst.append( ledSetEye.ledSetEye() )
+			# get files in dir
+			for f in os.listdir(d):
+				modulePath = d.replace("/", ".") + "."
+				
+				# import module
+				if f.endswith(".py") and not f.startswith("__") and not f.startswith("_"):
+					moduleName = modulePath + f.replace(".py", "")
+					module = __import__( moduleName, globals(), locals(), ['object'], -1 )
+				
+					# get classes from module
+					for name, cls in inspect.getmembers( module, inspect.isclass ):
+						if modulePath in cls.__module__:
+						
+							try:
+							
+								# check if class is command class
+								obj = cls()
+								if getattr( obj, 'cmd', None ):
+									NAOCommand.lst.append( obj )	# add to command list
+									logging.debug( "imported %s", cls )
+									
+							except Exception, e:
+								logging.error( "cannot import %s\n%s", cls, e )
 
 
 	'''
